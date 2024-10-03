6becb7206f69c73e84a73f56d249282072914053
Mr.Who <hjh19990814@gmail.com>
2024-10-03 03:08:02 +0200

    add docker-compose.yml

--- frontend/src/components/LoginPageComponent/Button.jsx
+++ frontend/src/components/LoginPageComponent/Button.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import './Button.css'; // Import the CSS file without assigning it to `styles`
 
-function Button({ children, type }) {
+function Button({ children, type = 'button', disabled = false }) {
     return (
-        <button type={type} className="button"> {/* Use the class name as a string */}
+        <button
+            type={type}
+            className={`px-4 py-2 bg-orange-400 hover:bg-orange-600-600 text-white font-bold rounded-lg transition-colors duration-300 ease-in-out 
+            ${disabled ? 'opacity-50 cursor-not-allowed' : ''}`}
+            disabled={disabled}
+        >
             {children}
         </button>
     );
